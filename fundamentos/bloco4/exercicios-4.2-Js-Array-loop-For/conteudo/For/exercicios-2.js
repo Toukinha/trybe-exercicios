57b67ee1a303644bcd074e5e121a87222d2fc414
Luis Gustavo|Toukinha <gustavoluis.pixell@gmail.com>
2022-07-21 15:36:52 -0300

    correcao de atividades

---
+++ fundamentos/bloco4/exercicios-4.2-Js-Array-loop-For/conteudo/For/exercicios-2.js
@@ -0,0 +1,12 @@
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+
+// console.log(names[2]);
+
+// // for (let printNames of names){
+// //     console.log(printNames);
+// // }
+for(let index = 0; index < 10; index+=1 ){
+    console.log(index);
+  }
+  
---
+++ fundamentos/bloco4/exercicios-4.2-Js-Array-loop-For/conteudo/array-list/exercicio-1.js
@@ -0,0 +1,4 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf('Serviços');
+
+console.log(menu[1]);
