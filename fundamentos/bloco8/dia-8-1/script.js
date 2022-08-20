7b13507b1cd5d0646cc3ff36d81e519a9ea1b29b
Luis Gustavo|Toukinha <gustavoluis.pixell@gmail.com>
2022-08-20 19:02:36 -0300

    exercicio 1

---
+++ fundamentos/bloco8/dia-8-1/script.js
@@ -0,0 +1,24 @@
+const newEmployees = () => {
+  const employees = {
+    id1: email('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: email('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: email('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+const email = (name) => {
+  const splitJoin = () => {  
+    name = name.split(' ')
+      .join('')
+        .toLowerCase();
+    
+    return name
+};
+  const emailg = {
+     nome: name,
+     email: `${splitJoin()}@trybe.com.`
+   };
+   return emailg
+  };
+  console.log(newEmployees());
